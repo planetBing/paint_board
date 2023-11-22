b6781b77604b9c4a6b00e0f39b8c031e763c67c4
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2023-11-22 23:54:06 +0900

    Print square and border

--- stage1_progressProgram.js
+++ stage1_progressProgram.js
@@ -3,14 +3,17 @@ const {
     receiveFirstInput,
     receiveSecondInput,
 } = require("./stage1_inputController.js");
+const { viewBorderAndSquare, printLine } = require("./stage1_view.js");
 
 function progressProgram() {
+    printLine("사각형 그리기 프로그램");
     const firstInput = receiveFirstInput("첫 번째 점의 좌표를 입력하세요");
     const secondInput = receiveSecondInput(
         "두 번째 점의 좌표를 입력하세요",
         firstInput
     );
-    console.log(secondInput);
+    viewBorderAndSquare(firstInput, secondInput);
+    printLine("프로그램을 종료합니다.");
 }
 
 module.exports = { progressProgram };
--- stage1_view.js
+++ stage1_view.js
@@ -2,6 +2,34 @@ function printLine(text) {
     console.log(text);
 }
 
-function printWithoutLine(text) {
-    process.stdout.write(text);
+function repeatPrint(string, repetitionNum) {
+    for (let i = 1; i <= repetitionNum; i++) {
+        printLine(string);
+    }
 }
+
+const upDownBorderStirng = "-".repeat(80);
+const sideBorderString = "|" + " ".repeat(78) + "|";
+
+function viewBorderAndSquare(firstInput, secondInput) {
+    const [X1, Y1] = firstInput;
+    const [X2, Y2] = secondInput;
+    printLine(upDownBorderStirng);
+    repeatPrint(sideBorderString, Y1 - 1);
+    const squareString = splitSquareIntoString(X1, X2);
+    repeatPrint(squareString, Y2 - Y1);
+    repeatPrint(sideBorderString, 28 - Y2 + 1);
+    printLine(upDownBorderStirng);
+}
+
+function splitSquareIntoString(X1, X2) {
+    return (
+        "|" +
+        " ".repeat(X1 - 1) +
+        "*".repeat(X2 - X1) +
+        " ".repeat(78 - X2 + 1) +
+        "|"
+    );
+}
+
+module.exports = { viewBorderAndSquare, printLine };
